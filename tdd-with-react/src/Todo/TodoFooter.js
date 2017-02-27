d55d21cc6dec0d080ca1952fe88184865d1218fb
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2017-02-27 23:22:54 +0700

    Wired up filter

--- tdd-with-react/src/Todo/TodoFooter.js
+++ tdd-with-react/src/Todo/TodoFooter.js
@@ -5,6 +5,7 @@ const propTypes = {
   filter: React.PropTypes.string.isRequired,
   count: React.PropTypes.number.isRequired,
   onClearCompleted: React.PropTypes.func,
+  onToggleFilter: React.PropTypes.func,
 }
 const defaultProps = {
   filter: 'all',
@@ -12,41 +13,54 @@ const defaultProps = {
   onClearCompleted: () => { },
 }
 
-const TodoFooter = props => (
-  <footer className="footer">
-    <span className="todo-count">
-      <strong>{props.count}</strong> item(s) left
-    </span>
-    <ul className="filters">
-      <li>
-        <span
-          className={classNames('ax-all', {
-            selected: props.filter === 'all'
-          })}>
-            All
-        </span>
-      </li>
-      {' '}
-      <li>
-        <span
-          className={classNames('ax-active', {selected: props.filter === 'active'})}>
-            Active
-        </span>
-      </li>
-      {' '}
-      <li>
-        <span className={classNames('ax-completed', {selected: props.filter === 'completed'})}>
-            Completed
-        </span>
-      </li>
-    </ul>
-    <button
-      className="clear-completed"
-      onClick={props.onClearCompleted}>
-        Clear completed
-    </button>
-  </footer>
-)
+const TodoFooter = props => {
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>{props.count}</strong> item(s) left
+      </span>
+      <ul className="filters">
+        <li>
+          <span
+            className={classNames('ax-all', {
+              selected: props.filter === 'all'
+            })}
+            onClick={() => props.onToggleFilter('all')}
+          >
+              All
+          </span>
+        </li>
+        {' '}
+        <li>
+          <span
+            className={classNames('ax-active', {
+              selected: props.filter === 'active'
+            })}
+            onClick={() => props.onToggleFilter('active')}
+          >
+              Active
+          </span>
+        </li>
+        {' '}
+        <li>
+          <span
+            className={classNames('ax-completed', {
+              selected: props.filter === 'completed'
+            })}
+            onClick={() => props.onToggleFilter('completed')}
+          >
+              Completed
+          </span>
+        </li>
+      </ul>
+      <button
+        className="clear-completed"
+        onClick={props.onClearCompleted}>
+          Clear completed
+      </button>
+    </footer>
+  )
+}
 
 TodoFooter.propTypes = propTypes
 TodoFooter.defaultProps = defaultProps
